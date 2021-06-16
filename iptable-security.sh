c12874c7d9ff497f9057303dc30fe7c89dbfc2b6
Mohammad Javadi <s.m.javadi001@gmail.com>
2021-06-16 16:03:23 +0430

    add service file

--- iptable-security.sh
+++ iptable-security.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in $(netstat -ntpl | gawk '{ print $4}' | grep -v 18080 | gawk -F ":" '{print $NF}' | grep -v "^$" | sort | grep -v [a-z] | uniq)
+for i in $(netstat -ntpl | awk '{ print $4}' | grep -v 18080 | awk -F ":" '{print $NF}' | grep -v "^$" | sort | grep -v [a-z] | uniq)
 do
-	for j in $(cat ./ipwhitelist.txt)
+	for j in $(cat /root/iptable-roles/ipwhitelist.txt)
 	do
 		iptables -A INPUT -p tcp --dport $i -s $j -j ACCEPT
 		iptables -A INPUT -p udp --dport $i -s $j -j ACCEPT
