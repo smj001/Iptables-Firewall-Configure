5c9ff3459248f67ec028b8bacbb23f1bb8a638fe
smj001 <s.m.javadi001@gmail.com>
2022-01-05 12:47:59 +0330

    add log to script

--- iptable-security.sh
+++ iptable-security.sh
@@ -9,16 +9,23 @@ then
      iptables -F INPUT
 fi
 
+echo "access list:"
 for i in $(netstat -ntpl | awk '{ print $4}' | grep -v 18080 | awk -F ":" '{print $NF}' | grep -v "^$" | sort | grep -v [a-z] | uniq)
 do
 	for j in $(cat ipwhitelist.txt | sed '/^#/d' | sed '/^$/d')
 	do
 		iptables -A INPUT -p tcp --dport $i -s $j -j ACCEPT
+        echo "$j --> $i/tcp"
 		iptables -A INPUT -p udp --dport $i -s $j -j ACCEPT
+        echo "$j --> $i/udp"
 	done
     iptables -A INPUT -p tcp --dport $i -j DROP
+    echo "Drop all TCP connections"
 	iptables -A INPUT -p udp --dport $i -j DROP
+    echo "Drop all UDP connections"
 done
 
 iptables -A INPUT -s 65.21.13.226 -p ICMP --icmp-type 8 -j ACCEPT
+echo "ICMP allow to 65.21.13.226"
 iptables -A INPUT -p ICMP --icmp-type 8 -j DROP
+echo "Drop All ICMP Connections"
